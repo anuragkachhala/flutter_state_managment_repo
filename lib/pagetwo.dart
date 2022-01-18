2858e5437cb83669c99261b813f45c5a63bc5c32
anurag <kachhalaanurag@gmail.com>
2022-01-18 10:43:26 +0530

    login_example

---
+++ lib/pagetwo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Page 2"),
+      ),
+      body: const Center(
+        child: Text("Page 2"),
+      ),
+    );
+  }
+}
